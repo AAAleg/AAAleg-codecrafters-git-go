2013183e7480c34d3486e04b380a3898c997ec56
Oleg Marintsov <awwert@yandex.ru>
2023-03-19 08:25:07 +0700

    Read blob object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -26,7 +29,49 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+	case "cat-file":
+		blobSha := os.Args[3]
+		blob, err := os.Open(fmt.Sprintf(".git/objects/%s/%s", blobSha[0:2], blobSha[2:]))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading blob: %s\n", err)
+		}
+		defer func() {
+			if err = blob.Close(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error closing blob: %s\n", err)
+			}
+		}()
 
+		blobData := bufio.NewReader(blob)
+
+		dataReader, err := zlib.NewReader(blobData)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading blob data: %s\n", err)
+		}
+		defer dataReader.Close()
+
+		objectType := make([]byte, 1)
+		for {
+			n, err := dataReader.Read(objectType)
+			if n > 0 {
+				t := string(objectType[:n])
+				if t == "\x00" {
+					break
+				}
+			}
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error reading type: %s\n", err)
+				break
+			}
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading blob type: %s\n", err)
+		}
+
+		io.Copy(os.Stdout, dataReader)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
